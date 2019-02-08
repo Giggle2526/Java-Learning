70ebbb38eca6f36436d6f6f88ea5d25a53155c26
LiuHuan <giggle1929@gmail.com>
2019-02-08 18:51:43 +0800

    020819

---
+++ 1.3/Steque.java
@@ -0,0 +1,13 @@
+import edu.princeton.cs.algs4.*;
+
+public class Stdque<Item> implements Iterable<Item>
+{
+	
+	public void push(Item item){
+
+	}
+	public Iterable<Item>
+	public static void main(String[] args) {
+		
+	}
+}
