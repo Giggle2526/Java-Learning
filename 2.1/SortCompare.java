247b881e46c49df2bedc4ed83337e0d9e115c35a
LiuHuan <giggle1929@gmail.com>
2019-02-10 23:31:17 +0800

    021019

--- 2.1/SortCompare.java
+++ 2.1/SortCompare.java
@@ -21,8 +21,16 @@ public class SortCompare{
 				a[i] = StdRandom.uniform();
 			total += time(alg, a);
 		}
+		return total;
 	}
 	public static void main(String[] args) {
-		
+		String alg1 = args[0];
+		String alg2 = args[1];
+		int N = Integer.parseInt(args[2]);
+		int T = Integer.parseInt(args[3]);
+		double t1 = timeRandomInput(alg1, N, T);
+		double t2 = timeRandomInput(alg2, N, T);
+		StdOut.printf("For %d random Doubles\n 	%s is", N, alg1);
+		StdOut.printf(" %.1f times faster than %s\n" , t2/t1, alg2);
 	}
 }
